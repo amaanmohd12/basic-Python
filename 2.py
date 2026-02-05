ced17e8630db47702d49a30272e8c120d19ac9a0
amaanmohd12 <amaansmarty1010@gmail.com>
2026-02-05 23:34:01 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,6 @@
+count=0
+sum=0
+while count<11 :
+    count+=1
+    sum+=count
+print(sum)    
---
+++ 4.py
@@ -0,0 +1,9 @@
+num=int(input("Enter a number:"))
+fact=1
+i=1
+while i <=num:
+    fact=fact*i
+    i+=1
+print("The Factorial of number",num,"is",fact)    
+
+    
