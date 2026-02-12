5fb7491cf16209a27cf084e0967bd6c1bbb60003
Mohd Amaan <amaansmarty1010@gmail.com>
2026-02-12 23:50:24 +0530

    Create Function.py

---
+++ Function.py
@@ -0,0 +1,8 @@
+def max():
+    a=int(input("Enter the first number:"))
+    b=int(input("Enter the second number:"))
+    if(a>b):
+        print(a,"is maximum")
+    else:
+        print(b," is maximum")    
+max()        
