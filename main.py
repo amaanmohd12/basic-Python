f1af240e6ad37b5d9a1763dedc3291a056c2affd
Mohd Amaan <amaansmarty1010@gmail.com>
2026-02-18 23:30:48 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+
+import mymath
+print("Addition:",mymath.add(10,5))
+print("Multiplication:",mymath.multiply(10,5))
+print("Subtraction:",mymath.subtract(10,5))
+print("Divide:",mymath.divide(10,5))
+print("Square",mymath.square(2))
+print("Cube:",mymath.cube(2))
+print("C to F:",mymath.c_to_f(10))
+print("F to C",mymath.f_to_c(50))
