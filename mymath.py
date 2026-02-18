372e6298410fff2e7dd3236ecc7b1f3b2dc5d425
Mohd Amaan <amaansmarty1010@gmail.com>
2026-02-18 23:30:05 +0530

    Create mymath.py

---
+++ mymath.py
@@ -0,0 +1,18 @@
+def add(a,b):
+    return a+b
+def subtract(a,b):
+    return a-b
+def multiply(a,b):
+    return a*b
+def divide(a,b):
+    return a/b
+def square(num):
+    return num*num
+def cube(num):
+    return num*num*num
+def c_to_f(c):
+    return (c*9/5)+32
+def f_to_c(f):
+    return (f-32)*5/9
+
+PI=3.14159
