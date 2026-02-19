6e36595a73682059e981dfbc2843b5b239d9072d
Mohd Amaan <amaansmarty1010@gmail.com>
2026-02-19 22:54:11 +0530

    Create Rock_paper_scissors_game.py

---
+++ Rock_paper_scissors_game.py
@@ -0,0 +1,13 @@
+import random 
+choices=['rock','paper','scissors']
+user=input("Enter rock,paper or scissors:") .lower()
+computer=random.choice(choices)
+print("Computer chose:",computer)
+if user==computer:
+    print("It is a Tie!")
+elif(user=='rock' and computer=='scissors') or\
+    (user=='paper' and computer=='rock') or\
+    (user=='scissors' and computer=='paper'):
+    print("You Win!")
+else:
+    print("The Computer Won!")    
